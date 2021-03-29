f88baa9fadb68181205e0a20e6f6373022eaa518
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-03-29 11:51:49 -0500

    Adding author information and reaction buttons. The reaction buttons aren't showing up yet.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,24 +1,44 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'First Post !', content: 'Hello, World!' },
-    { id: '2', title: 'Second Post', content: 'More Text' },
+    { id: '1',
+        title: 'First Post !',
+        content: 'Hello, World!',
+        date: sub(new Date(), { minutes: 10 }).toISOString(),
+        reactions: {thumbsUp: '0', hooray: '0', heart: '0', rocket: '0', eyes: '0'}
+        },
+    { id: '2',
+        title: 'Second Post',
+        content: 'More Text',
+        date: sub(new Date(), { minutes: 5 }).toISOString(),
+        reactions: {thumbsUp: '0', hooray: '0', heart: '0', rocket: '0', eyes: '0'}
+        }
 ]
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
+        reactAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
+            }
+        },
         postAdded: {
             reducer(state, action) {
                 state.push(action.payload)
             },
-            prepare(title, content) {
+            prepare(title, content, userId) {
                 return {
                     payload: {
                         id: nanoid(),
+                        date: new Date().toISOString(),
                         title,
-                        content
+                        content,
+                        user: userId
                     }
                 }
             }
@@ -37,6 +57,6 @@ const postsSlice = createSlice({
     }
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
