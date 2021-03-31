fe23731e79e312a3e9c818b8e745255a64d87c0c
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-03-30 20:33:20 -0500

    adding notifications tab, showing unread notifications stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-
-import { Navbar } from './app/Navbar'
-
-import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
-import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+import { Navbar } from './app/Navbar'
+import { NotificationsList } from './features/notifications/NotificationsList'
+import { PostsList } from './features/posts/PostsList'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 import { UsersList } from './features/users/UsersList'
 import { UserPage } from './features/users/UserPage'
+
+
 
 function App() {
   return (
@@ -26,8 +27,9 @@ function App() {
                   </React.Fragment>
               )}
               />
-              <Route exact path="/posts/:postId" component={SinglePostPage} />
               <Route exact path="/editPost/:postId" component={EditPostForm} />
+              <Route exact path="/notifications" component={NotificationsList} />
+              <Route exact path="/posts/:postId" component={SinglePostPage} />
               <Route exact path="/users" component={UsersList} />
               <Route exact path="/users/:userId" component={UserPage} />
               <Redirect to="/" />
