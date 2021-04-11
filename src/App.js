bede3a088e9310f5276a6fe485ce102392dfe327
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-04-11 16:39:32 -0500

    new plan <exclaimation point/>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    posts: []
+  }
+
+  async componentDidMount() {
+    const response = await fetch("/api/posts")
+    const body = await response.json()
+    this.setState({posts: body.posts, isLoading: false})
+  }
+
+  render() {
+    const { posts, isLoading } = this.state
+    if (isLoading) {
+      return <p>Loading...</p>
+    }
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="App-intro">
+              <h2>Posts</h2>
+              {posts.map(post =>
+              <div key={post.postId}>
+                <p>{post.userId}</p>
+                <p>{post.postTitle}</p>
+                <p>{post.postContent}</p>
+              </div>
+              )}
+            </div>
+          </header>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
